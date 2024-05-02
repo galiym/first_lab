08b495796f4d72390b60a068a7c8161562e5bea4
Tesssl0 <131177094+Tesssl0@users.noreply.github.com>
2024-05-02 21:30:49 +0700

    Update  pryamougolnick.py

--- 123456.py
+++ 123456.py
@@ -1,35 +1,11 @@
-import math
-
-# Класс, представляющий прямоугольник
-class Rectangle:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
-    # Метод для вычисления периметра прямоугольника
-    def calculate_perimeter(self):
-        return 2 * (self.width + self.height)
-
-    # Метод для вычисления площади прямоугольника
-    def calculate_area(self):
-        return self.width * self.height
-
-    # Метод для вычисления длины диагонали прямоугольника
-    def calculate_diagonal_length(self):
-        return math.sqrt(self.width ** 2 + self.height ** 2)
-
-# Основная программа
-if __name__ == "__main__":
-    print("Введите ширину прямоугольника:")
-    width = float(input())
-
-    print("Введите высоту прямоугольника:")
-    height = float(input())
-
-    # Создание объекта прямоугольника
-    my_rectangle = Rectangle(width, height)
-
-    # Вычисление и вывод параметров прямоугольника
-    print(f"Периметр прямоугольника: {my_rectangle.calculate_perimeter()}")
-    print(f"Площадь прямоугольника: {my_rectangle.calculate_area()}")
-    print(f"Длина диагонали прямоугольника: {my_rectangle.calculate_diagonal_length()}")
+def perimetr(a,b):
+    return (a+b)2
+def ploshad(a,b):
+    return ab
+def diagonal(a,b):
+    return round((a2+b2)**0.5, 2)
+def main():
+    a = int(input('введите ширину прямоугольника: '))
+    b = int(input('введите длину прямоугольника: '))
+    print("Периметр =", perimetr(a,b),"\nПлощадь =", ploshad(a,b), "\nДиагональ =", diagonal(a,b))
+main()
